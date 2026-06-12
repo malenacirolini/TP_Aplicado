68012a0c86d563a1c5201342b95a65dfd81c3dd6
sofimorenoc <smorenocoppaoliver@udesa.edu.ar>
2026-06-12 13:42:50 -0300

    a ver

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 11 17:19:54 2026
+
+@author: sofi
+"""
+
+from cinemood import main
+
+if __name__ == "__main__":
+    main() 
+    
